ce8ef802ccd2d96c81593af7c72f9e32463bdb35
Adam Peace <adam.n.peace@gmail.com>
2018-03-01 18:08:57 +0000

    file

--- qualifier.py
+++ qualifier.py
@@ -1,1 +1,14 @@
-import random
+import random, re
+fileName = "a_example.in"
+inputFile = open(fileName, mode='r')
+info = []
+count = 1
+def printArray(array):
+    for subarray in array:
+        print(subarray)
+for line in inputFile:
+    info.append(re.sub("[^\w]", " ",  line).split())
+        
+    count += 1
+
+printArray(info)
